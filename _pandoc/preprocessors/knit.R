56c893eb6217f3d002dd22efd46e74b01ad08ea4
André Bauer <bauerand@informatik.uni-marburg.de>
2022-10-13 20:43:27 +0200

    Add files to _pandoc.

---
+++ _pandoc/preprocessors/knit.R
@@ -0,0 +1,10 @@
+#!/usr/bin/Rscript
+sink(file("/dev/null", "w"), type="message")
+args <- commandArgs(trailingOnly = TRUE)
+library(docknitr)
+quiet <- FALSE
+if (length(args) > 2 && args[3] == "quiet") {
+   quiet <- TRUE
+}
+
+invisible(knitr::knit(args[1], args[2], quiet=quiet))
