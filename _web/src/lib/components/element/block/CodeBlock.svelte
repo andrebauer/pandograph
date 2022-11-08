7277096bd9d1f8c7e7601996647c3b9b0c7c40ea
André Bauer <bauerand@informatik.uni-marburg.de>
2022-11-08 07:18:21 +0100

    Use highlight insted of prism.

--- _web/src/lib/components/element/block/CodeBlock.svelte
+++ _web/src/lib/components/element/block/CodeBlock.svelte
@@ -1,14 +1,14 @@
 <script lang="ts" context="module">
-	import Prism from 'prismjs';
+	/*import Prism from 'prismjs';
 	import 'prismjs/themes/prism.css';
 	import 'prismjs/plugins/line-highlight/prism-line-highlight.css';
-	import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+	import 'prismjs/plugins/line-numbers/prism-line-numbers.css';*/
 	/* import 'prismjs/plugins/line-highlight/prism-line-highlight';
 	import 'prismjs/plugins/line-numbers/prism-line-numbers'; */
 	// Prism.manual = true;
-	import loadLanguages from 'prismjs/components/';
-	// import hljs from 'highlight.js';
-	// import 'highlight.js/styles/github.css';
+	// import loadLanguages from 'prismjs/components/';
+	import hljs from 'highlight.js';
+	import 'highlight.js/styles/github.css';
 
 	import { onMount } from 'svelte';
 	// import 'prismjs/plugins/'
@@ -24,8 +24,9 @@
 	id = id || '';
 	let pre: any;
 	// let highlightRanges = pre.get('data-line');
-	loadLanguages([language]);
+	// loadLanguages([language]);
 
+	/*
 	onMount(async () => {
 		await import('prismjs/plugins/line-highlight/prism-line-highlight');
 		await import('prismjs/plugins/line-numbers/prism-line-numbers');
@@ -33,6 +34,7 @@
 		// const Prism = document.Prism;
 		// loadLanguages([language]);
 	});
+*/
 
 	let hll: any;
 
@@ -50,12 +52,12 @@
 <div class="line-numbers bg-slate-50 p-4 rounded rounded-lg">
 	<pre bind:this={pre} {id} data-line="2" data-start="1" class="line-numbers language-{language}">
 <code class="language-{language}"
-			><!-- {@html hljs.highlight(source, { language })
-				.value} -->{@html Prism.highlight(
+			>{@html hljs.highlight(source, { language })
+				.value}<!--{@html Prism.highlight(
 				source,
 				Prism.languages[language],
 				language
-			)} 
+			)}-->
 </code></pre>
 </div>
 
