62ba6d6179640a9771b17ebf7a6714045f20de17
André Bauer <bauerand@informatik.uni-marburg.de>
2022-11-24 06:55:43 +0100

    Allow build context specific suffixes.

--- _waf/pandoc.py
+++ _waf/pandoc.py
@@ -353,7 +353,7 @@ def build(bld):
 
             for kind in kinds:
                 default = os.sep.join(bld.env.meta[srcpath][kind]['defaults'])
-                suffix = bld.env.suffix
+                suffix = bld.suffix or bld.env.suffix
                 variables = bld.env.yaml[default].get('variables')
                 if variables:
                     suffix = variables.get('suffix') or suffix
