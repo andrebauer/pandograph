768d44bfc94c26ab166c7a8fd8aee46a0aff824b
André Bauer <bauerand@informatik.uni-marburg.de>
2022-12-23 09:32:11 +0100

    Add options to preprocessor.

--- _pandoc/preprocessors/knit.R
+++ _pandoc/preprocessors/knit.R
@@ -1,4 +1,6 @@
 ## #!/usr/bin/env Rscript
+# Requirements: kntir, docknitr
+# Additional requirements when unsing tikz with knitr: magick, pdftools
 # sink(file("/dev/null", "w"), type="message")
 args <- commandArgs(trailingOnly = TRUE)
 library(docknitr, quietly = TRUE)
@@ -6,5 +8,6 @@ quiet <- FALSE
 if (length(args) > 2 && args[3] == "quiet") {
    quiet <- TRUE
 }
-
+options(texi2dvi = "lualatex")
+options(tinytex.engine = "lualatex")
 invisible(knitr::knit(args[1], args[2], quiet=quiet))
