fe8a941090eb36a5155e7d8a9e7aabe1157431f2
André Bauer <bauerand@informatik.uni-marburg.de>
2023-02-02 00:11:44 +0100

    Refine filter frame.lua.

--- _pandoc/filters/pfs/frame/frame.lua
+++ _pandoc/filters/pfs/frame/frame.lua
@@ -6,14 +6,21 @@ package.path = fmt("%s;%s/../?.lua", package.path, pandoc_script_dir)
 require 'lib.latex'
 
 function Div(div)
+  local blocks = pandoc.List(pandoc.Blocks(div.content))
   if div.classes[1] == 'file' or div.classes[1] == 'frame' then
     if FORMAT:match 'latex' or FORMAT:match 'beamer' then
-      local blocks = pandoc.List(pandoc.Blocks(div.content))
       blocks:insert(1, block_latex_fmt('\\begin{%s}{%s}',
                                        'filebox',
                                        div.attributes.title))
       blocks:insert(block_latex_end('filebox'))
       return blocks
     end
+  end
+
+  if div.classes[1] == 'file' then
+    blocks:insert(1, pandoc.read(
+                    "Die Datei `" .. div.attributes.title .. "`:")
+                    .blocks[1])
+    return blocks
   end
 end
